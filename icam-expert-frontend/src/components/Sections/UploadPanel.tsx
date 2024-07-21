1ac4f25d9f35c1fd1f38d41e80dba5c3373ae998
caglarcem <caglarcem@gmail.com>
2024-07-21 15:31:09 +1000

    added settings section

--- icam-expert-frontend/src/components/Sections/UploadPanel.tsx
+++ icam-expert-frontend/src/components/Sections/UploadPanel.tsx
@@ -1,20 +1,30 @@
 import {
+  ExpandLess as ExpandLessIcon,
+  ExpandMore as ExpandMoreIcon,
   RemoveCircleOutline as RemoveIcon,
+  Settings as SettingsIcon,
   UploadOutlined as UploadIcon,
 } from '@mui/icons-material';
 import {
   Box,
   Button,
+  FormControl,
   IconButton,
   InputLabel,
+  MenuItem,
+  Select,
   Tooltip,
   Typography,
 } from '@mui/material';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useFileContext } from '../../context/FileProvider';
 
 const UploadPanel: React.FC = () => {
   const { files, setFiles } = useFileContext();
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [state, setState] = useState('QLD'); // Default to QLD
+  const [mineType, setMineType] = useState('Open cut'); // Default to Open cut
+  const [commodity, setCommodity] = useState('Coal'); // Default to Coal
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -49,8 +59,169 @@ const UploadPanel: React.FC = () => {
     ...videoFileTypes,
   ].join(', ');
 
-  return (
-    <Box sx={{ padding: 2 }}>
+  const handleSettingsClick = () => {
+    setIsSettingsOpen(!isSettingsOpen);
+  };
+
+  const fixedWidth = 250; // Set a fixed width for the elements
+
+  const Settings: React.FC = () => (
+    <Box sx={{ padding: 2, paddingBottom: 0, width: fixedWidth }}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          marginBottom: 1,
+          width: '100%',
+        }}
+      >
+        <Button
+          onClick={handleSettingsClick}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            textTransform: 'none',
+            padding: 0,
+            fontSize: '0.875rem',
+            color: '#003366', // Darker blue color
+            '&:hover': {
+              color: '#002244', // Darker shade on hover
+            },
+            width: '100%',
+          }}
+        >
+          <SettingsIcon sx={{ marginRight: 1 }} />
+          <Typography variant="body1">Settings</Typography>
+          {isSettingsOpen ? (
+            <ExpandLessIcon sx={{ marginLeft: 'auto' }} />
+          ) : (
+            <ExpandMoreIcon sx={{ marginLeft: 'auto' }} />
+          )}
+        </Button>
+      </Box>
+      {isSettingsOpen && (
+        <Box sx={{ marginBottom: 1, width: '100%' }}>
+          <FormControl
+            fullWidth
+            variant="outlined"
+            size="small"
+            sx={{ marginBottom: 1, marginTop: 1 }}
+          >
+            <InputLabel htmlFor="state-select" sx={{ fontSize: '0.85rem' }}>
+              State
+            </InputLabel>
+            <Select
+              id="state-select"
+              value={state}
+              onChange={e => setState(e.target.value as string)}
+              label="State"
+              sx={{ fontSize: '0.85rem' }}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    fontSize: '0.85rem',
+                    width: 200,
+                  },
+                },
+              }}
+            >
+              <MenuItem value="QLD" sx={{ fontSize: '0.85rem' }}>
+                QLD
+              </MenuItem>
+              <MenuItem value="NT" sx={{ fontSize: '0.85rem' }}>
+                NT
+              </MenuItem>
+              <MenuItem value="NSW" sx={{ fontSize: '0.85rem' }}>
+                NSW
+              </MenuItem>
+              <MenuItem value="VIC" sx={{ fontSize: '0.85rem' }}>
+                VIC
+              </MenuItem>
+              <MenuItem value="SA" sx={{ fontSize: '0.85rem' }}>
+                SA
+              </MenuItem>
+              <MenuItem value="WA" sx={{ fontSize: '0.85rem' }}>
+                WA
+              </MenuItem>
+              <MenuItem value="TAS" sx={{ fontSize: '0.85rem' }}>
+                TAS
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl
+            fullWidth
+            variant="outlined"
+            size="small"
+            sx={{ marginBottom: 1, marginTop: 1 }}
+          >
+            <InputLabel htmlFor="mine-type-select" sx={{ fontSize: '0.85rem' }}>
+              Mine Type
+            </InputLabel>
+            <Select
+              id="mine-type-select"
+              value={mineType}
+              onChange={e => setMineType(e.target.value as string)}
+              label="Mine Type"
+              sx={{ fontSize: '0.85rem' }}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    fontSize: '0.85rem',
+                    width: 200,
+                  },
+                },
+              }}
+            >
+              <MenuItem value="Open cut" sx={{ fontSize: '0.85rem' }}>
+                Open cut
+              </MenuItem>
+              <MenuItem value="Underground" sx={{ fontSize: '0.85rem' }}>
+                Underground
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl
+            fullWidth
+            variant="outlined"
+            size="small"
+            sx={{ marginTop: 1 }}
+          >
+            <InputLabel htmlFor="commodity-select" sx={{ fontSize: '0.85rem' }}>
+              Commodity
+            </InputLabel>
+            <Select
+              id="commodity-select"
+              value={commodity}
+              onChange={e => setCommodity(e.target.value as string)}
+              label="Commodity"
+              sx={{ fontSize: '0.85rem' }}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    fontSize: '0.85rem',
+                    width: 200,
+                  },
+                },
+              }}
+            >
+              <MenuItem value="Coal" sx={{ fontSize: '0.85rem' }}>
+                Coal
+              </MenuItem>
+              <MenuItem value="Metal" sx={{ fontSize: '0.85rem' }}>
+                Metal
+              </MenuItem>
+              <MenuItem value="Quarry" sx={{ fontSize: '0.85rem' }}>
+                Quarry
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      )}
+    </Box>
+  );
+
+  const SelectFiles: React.FC = () => (
+    <Box sx={{ padding: 2, width: fixedWidth }}>
       <input
         id="fileInput"
         style={{ display: 'none' }}
@@ -60,13 +231,15 @@ const UploadPanel: React.FC = () => {
         multiple
         data-testid="file-input"
       />
-      <InputLabel htmlFor="fileInput">
+      <InputLabel htmlFor="fileInput" sx={{ width: '100%' }}>
         <Button
           sx={{
             backgroundColor: '#008009',
             '&:hover': {
               backgroundColor: '#36A137',
             },
+            marginTop: 0,
+            width: '100%',
           }}
           variant="contained"
           component="span"
@@ -102,6 +275,13 @@ const UploadPanel: React.FC = () => {
           </Box>
         ))}
       </Box>
+    </Box>
+  );
+
+  return (
+    <Box>
+      <Settings />
+      <SelectFiles />
     </Box>
   );
 };
